b469a0d7c63f66d893277447d726369157529cac
Matheus Milanez <mattmila96@gmail.com>
2023-05-08 22:23:47 -0300

    requisito 8

---
+++ challenges/desafio8.sql
@@ -0,0 +1,5 @@
+SELECT art.nome AS artista, alb.nome AS album FROM SpotifyClone.albuns AS alb
+INNER JOIN SpotifyClone.artistas AS art
+ON alb.artista_id = art.artista_id
+WHERE art.nome = 'Elis Regina'
+ORDER BY album;
